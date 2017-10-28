7b15ab5d5ea5ca59c13b4263a5b22bbbf3a6ce63
Julio Foulquie <jfoulquie@gmail.com>
2017-10-28 18:01:54 -0300

    Add jquery and load the geojson.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 import Accordion from 'rollerskate';
 const accordion = new Accordion(window);
 accordion.collapseAll();
+
+import jquery from 'jquery';
+window.$ = window.jQuery = jquery;
+
+$.getJSON("mujeres.geojson", function(geojson) {
+    // geojson has all the data
+});
