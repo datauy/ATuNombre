a8f25f9e15ce6fe3bdf41c904660abc8bf592021
Julio Foulquie <jfoulquie@gmail.com>
2017-11-21 00:32:17 -0300

    Add piechart module.

---
+++ src/js/piechart.js
@@ -0,0 +1,43 @@
+export default function(selector, data_source) {
+
+var svg = d3.select(selector),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    radius = Math.min(width, height) / 2,
+    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var fader = function(color) {
+            return d3.interpolateRgb(color, '#fff')(0.2);
+        },
+        color = d3.scaleOrdinal(d3.schemeCategory10.map(fader));
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.value; });
+
+var path = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var label = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+d3.json(data_source,function(error, data) {
+  if (error) throw error;
+
+  var arc = g.selectAll(".arc")
+    .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  arc.append("path")
+      .attr("d", path)
+      .attr("fill", function(d) { return color(d.data.name); });
+
+  arc.append("text")
+      .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+      .attr("dy", "0.35em")
+      .text(function(d) { return d.data.name; });
+});
+}
